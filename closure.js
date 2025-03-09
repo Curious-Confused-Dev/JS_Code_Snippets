1811c9e98f283ece1a18706466dfc687ca01a655
@anshdeep.chawla <anshdeep.chawla@accenture.com>
2025-03-09 21:12:17 +0530

    Closures

---
+++ closure.js
@@ -0,0 +1,56 @@
+function outerFunction() {
+    let outervar = 'Hi! I am from outer function';
+
+    function innerFunction() {
+        console.log(outervar);
+    }
+
+    return innerFunction; //Returns reference to Outer Function
+}
+
+
+let closurefun = outerFunction();
+
+closurefun();
+
+//Closures are often used to create private variables in JavaScript.
+function counter() {
+    let count = 0; //Private Variable
+
+    return {
+        increment: function() {
+            count ++;
+            console.log(count);
+        },
+
+        decrement: function() {
+            count --;
+            console.log(count);
+        }
+
+    };
+}
+
+const myCounter = counter();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.decrement();
+myCounter.decrement();
+myCounter.decrement();
+myCounter.decrement();
+myCounter.decrement();
+myCounter.decrement();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+// Why is this useful?
+
+// count is not directly accessible outside the function.
+// It can only be changed using increment() or decrement(), keeping it safe from accidental modification.
